1798a8546decf32c81260f12cc1f243b8f3e7a5c
大宮　直樹　 <naoki_oomiya@noomiya.local>
2021-01-20 03:04:47 +0900

    update style

--- public/javascripts/dist/script.dev.js
+++ public/javascripts/dist/script.dev.js
@@ -4,10 +4,14 @@ function youtube_defer() {
   var iframe = document.querySelector('iframe');
   var url = iframe.getAttribute('data-src');
 
-  if (url) {
+  if (url.includes('youtube')) {
     var id = url.split('watch?v=')[1];
     var embedUrl = 'https://www.youtube.com/embed/' + id;
     iframe.setAttribute('src', embedUrl);
+  } else if (url.includes('soundcloud')) {
+    var _embedUrl = 'https://w.soundcloud.com/player/?visual=true&url=' + url;
+
+    iframe.setAttribute('src', _embedUrl);
   }
 }
 
